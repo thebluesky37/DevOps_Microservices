6c02c383d61fcafe2cc790e451f73cad45c31e13
Mhrovatic <mhrovatic@globalblue.com>
2023-06-14 21:37:45 +0700

    Complete .sh files

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --platform linux/amd64 -t predictapp:latest .
 
-# Step 2: 
+# Step 2:
 # List docker images
+docker images
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run -p 80:80 predictapp:latest
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=thebluesky37/udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predict-app-container --image=$dockerpath --port=80 --labels app=predict-app
 
 # Step 3:
 # List kubernetes pods
+kubectl wait --for=condition=Ready pod -l app=predict-app
+kubectl get pods predict-app-container
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predict-app-container 8080:80
+
 
